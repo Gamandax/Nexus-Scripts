c219bea6aa67a42ae70915d717a9538048e65ab4
Nexus <rodgiellamas@gmail.com>
2025-08-25 04:56:02 +0800

    Create All Script.lua

---
+++ All Script.lua
@@ -0,0 +1,209 @@
+-- Load Wave UI
+local Wave = loadstring(game:HttpGet("https://pastefy.app/5sEX03wA/raw"))()
+
+-- Create Window
+local window = Wave:Window("Aux Hub")
+
+-- Tabs
+local auxTab = window:Tab("Aux Script")
+local universalTab = window:Tab("Universal Aux Script")
+local espTab = window:Tab("Esp Aux Script")
+
+-- == Aux Script Tab ==
+auxTab:Label("Aux Script")
+
+auxTab:Button("+1 Cash Every Step.lua", function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/+1-Cash-Every-Step-OP-SCRIPT-Instant-Cash-Fast-Open-Source-47988"))()
+end)
+
+auxTab:Button("1+ Get Age per Click!.lua", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/1%2B%20Get%20Age%20per%20Click!.lua"))()
+end)
+
+auxTab:Button("1+ Speed Brainrot Escape.lua", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/1%2B%20Speed%20Brainrot%20Escape.lua"))()
+end)
+
+auxTab:Button("1+ Speed Fready Escape.lua", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/1%2B%20Speed%20Fready%20Escape.lua"))()
+end)
+
+auxTab:Button("99 nights in the forest", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/99%20night%20in%20the%20forest.lua"))()
+end)
+
+auxTab:Button("Brookhaven", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Brookhaven%20Aux%20hub.lua"))()
+end)
+
+auxTab:Button("Brainrot Clicker!", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Brainrot%20Clicker!.lua"))()
+end)
+
+auxTab:Button("Build a Brainrot", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Build%20A%20Brainrot.lua"))()
+end)
+
+auxTab:Button("Build a Plane", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Build%20a%20plane.lua"))()
+end)
+
+auxTab:Button("Build and Crash.lua", function()
+    loadstring(game:HttpGet("https://pastefy.app/SnuhajPD/raw"))()
+end)
+
+auxTab:Button("Jump With Brainrot", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Jump%20With%20Brainrot.lua"))()
+end)
+
+auxTab:Button("Invisible Hide and Seek", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Invisible%20Hide%20And%20Seek.lua"))()
+end)
+
+auxTab:Button("Grow Every Bite", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Grow%20Every%20Bite.lua"))()
+end)
+
+auxTab:Button("Funnel Tycoon 2", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Funnel%20Tycoon%202.lua"))()
+end)
+
+auxTab:Button("Create a Factory", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Create%20a%20factory.lua"))()
+end)
+
+auxTab:Button("Steal a Horse", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Steal%20A%20Horse.lua"))()
+end)
+
+auxTab:Button("Steal a Gear", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Steal%20A%20Gear.lua"))()
+end)
+
+auxTab:Button("Money Grabber Simulator", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Money%20grabber%20simulator.lua"))()
+end)
+
+auxTab:Button("Merge Gubby V1", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Merge%20Gubby.lua"))()
+end)
+
+auxTab:Button("Legend of Speed", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Legend%20Of%20Speed.lua"))()
+end)
+
+auxTab:Button("West Wood Bug [FIXED]", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/West%20Wood%20%5B%20Bug%20Fixed%20%5D.lua"))()
+end)
+
+auxTab:Button("Troll is a Pinning Tower 2", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Troll%20Is%20A%20Pinning%20Tower%202.lua"))()
+end)
+
+auxTab:Button("Trampoline Battle Simulator", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Trampoline%20Battle%20Simulator.lua"))()
+end)
+
+auxTab:Button("Steal a Slap [Cars]", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Steal%20A%20Slap%20%5BCars%5D.lua"))()
+end)
+
+auxTab:Button("Steal a Sword", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Steal%20A%20SWORD.lua"))()
+end)
+
+auxTab:Button("Push Glass Maker the glass bridge Challenge", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/%5BPUSH%20%26%20GLASS%20MAKER%5D%20The%20Glass%20Bridge.lua"))()
+end)
+
+auxTab:Button("Steal a Skibidi", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/%5BEvent%5D%20Steal%20A%20Sikibidi.lua"))()
+end)
+
+auxTab:Button("Brainrot Glass Bridge Challenge", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/%5BEASY%5D%20brainrot%20glass%20bridge%20challenge.lua"))()
+end)
+
+auxTab:Button("Get Big Simulator", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/%5B2x%F0%9F%92%AA%5D%20Get%20Big%20Simulator.lua"))()
+end)
+
+auxTab:Button("Find The Cans [120]", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/%5B120%5D%20Find%20The%20Cans.lua"))()
+end)
+
+
+-- == Universal Aux Script Tab ==
+
+universalTab:Label("Universal Script")
+
+universalTab:Button("WallHop", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/WallHop.lua"))()
+end)
+
+universalTab:Button("Player Teleporter Gui", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Teleport%20Player.lua"))()
+end)
+
+universalTab:Button("Telekinesis V5", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Telekinesis%20Script.lua"))()
+end)
+
+universalTab:Button("Coordinate Tool V3", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/Coordinate%20Tool.lua"))()
+end)
+
+universalTab:Button("JumpScare OP", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/JumpScare%20Script.lua"))()
+end)
+
+universalTab:Button("Soon!", function()
+    loadstring(game:HttpGet("ako_na_mag_lagay"))()
+end)
+
+universalTab:Button("Soon!", function()
+    loadstring(game:HttpGet("ako_na_mag_lagay"))()
+end)
+
+universalTab:Button("Soon!", function()
+    loadstring(game:HttpGet("ako_na_mag_lagay"))()
+end)
+
+universalTab:Button("Soon!", function()
+    loadstring(game:HttpGet("ako_na_mag_lagay"))()
+end)
+
+universalTab:Button("Soon!", function()
+    loadstring(game:HttpGet("ako_na_mag_lagay"))()
+end)
+
+universalTab:Button("Soon!", function()
+    loadstring(game:HttpGet("ako_na_mag_lagay"))()
+end)
+
+-- == Esp Aux Script Tab ==
+
+espTab:Label("All Collection of Aux ESP Scripts")
+
+espTab:Button("Nexus Esp", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Esp-Scripts-/refs/heads/main/Nexus%20Esp.lua"))()
+end)
+
+espTab:Button("NameTags Esp", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Esp-Scripts-/refs/heads/main/NameTags.lua"))()
+end)
+
+espTab:Button("Health Bar Esp", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Esp-Scripts-/refs/heads/main/Healthbar.lua"))()
+end)
+
+espTab:Button("Draw Box Esp", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Esp-Scripts-/refs/heads/main/Drawbox.lua"))()
+end)
+
+espTab:Button("Distance Esp", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Esp-Scripts-/refs/heads/main/Distance%20Esp.lua"))()
+end)
+
+
+print("Aux Hub Loaded ✅")
