c77ee2b1e54a7204c80e14eb492223d7ff4ad31a
Nexus <rodgiellamas@gmail.com>
2025-11-02 00:46:48 +0800

    Update CodeBox loading URL in Source.lua

--- VSCode/Source.lua
+++ VSCode/Source.lua
@@ -1147,7 +1147,7 @@ script.Parent.Visible = false
 script.Parent.Parent.Parent = getfenv().gethui and getfenv().gethui() or game:GetService("CoreGui") or game:GetService("Players").LocalPlayer.PlayerGui
 script.Parent.Size = size
 
-local codeBox = script:FindFirstChild("CodeBox") and require(script.CodeBox) or loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Null-Fire/main/Core/Libraries/CodeBox/Main.lua"))()
+local codeBox = script:FindFirstChild("CodeBox") and require(script.CodeBox) or loadstring(game:HttpGet("https://raw.githubusercontent.com/Gamandax/Nexus-Scripts/refs/heads/main/VSCode/CodeBox.lua"))()
 local textBox = codeBox:fromTextBox(script.Parent.Contents.CodeEditor, codeBox.Themes.VSCode, getfenv().getgenv and getfenv().getgenv())
 textBox.Text = [[-- VSCode by @cherry_peashooter on discord --
 
